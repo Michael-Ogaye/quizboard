8e15b4309f46894a8af6a8530723c108ae4ac35d
Michael Ogaye <hogaeymic@gmail.com>
2022-02-05 20:43:18 +0300

    bug: debugged-javascript code is running approriately on my browser console

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ buton.addEventListener("click",(e)=>{
     
     
 
-    var cnt = 0;
-    cnt++
+    var i = 0;
+    
     var q1 = document.quiz.qsn1.value;
     var q2 = document.quiz.qsn2.value;
     var q3 = document.quiz.qsn3.value;
     var q4 = document.quiz.qsn4.value;
     var q5 = document.quiz.qsn5.value;
     
-    if(q1 =="HTML"){cnt = cnt +10 ;}
-    if (q2 == "ECMAscript"){ cnt +=10;}
-    if (q3 =="Yes"){cnt +=10;}
-    if (q4 == "Yes"){cnt=+ 10;}
-    if(q5 =="Yes"){cnt =+ 10;}
+    if(q1 =="HTML"){ i += 10 }
+    if (q2 == "ECMAscript"){ i +=10}
+    if (q3 =="Yes"){i +=10}
+    if (q4 == "Yes"){i += 10}
+    if(q5 =="Yes"){i += 10}
 
 
     var score= document.getElementsByClassName('score')
-    score.innerHtml= `<p> You have scored ${ cnt }`;
+    score.innerHtml= `<p> You have scored ${ i }`;
     var forma= document.quiz;
     // forma.style.display = "none";
-    console.log(cnt);
+    console.log(i);
 })
 
