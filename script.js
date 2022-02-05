8a7a8dda166dd6f1f9fbcf257c5322e880e1da8c
Michael Ogaye <hogaeymic@gmail.com>
2022-02-05 14:19:34 +0300

    bug: Html-insertion-Html isertion from js failed

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ buton.addEventListener("click",(e)=>{
 
     var cnt = 0;
     var q1 = document.quiz.qsn1.value;
-    if(q1 =="HTML"){cnt = cnt +10 ;}
+    if(q1 =="HTML"){cnt += 10 ;}
     var q2 = document.quiz.qsn2.value;
-    if (q2 == "ECMAscript"){ cnt = cnt+10;}
+    if (q2 == "ECMAscript"){ var cnt1=cnt+10 ;}
     var q3 = document.quiz.qsn3.value;
-    if (q3 =="Yes"){cnt=+ 10;}
+    if (q3 =="Yes"){ var cnt2=cnt1+10;}
     var q4 = document.quiz.qsn4.value;
-    if (q4 == "Yes"){cnt=+ 10;}
+    if (q4 == "Yes"){ var cnt3= cnt2+10;}
     var q5 = document.quiz.value;
-    if(q5 =="Yes"){cnt =+ 10;}
+    if(q5 =="Yes"){var cnt4 =cnt3+ 10;}
     var score= document.getElementsByClassName('score')
-    score.innerHtml= `<p> You have scored ${ cnt }`;
+    score.innerHtml= `<p> You have scored ${ cnt4 }`;
     var forma= document.quiz;
     forma.style.display = "none";
+    console.log(cnt);
 })
 
